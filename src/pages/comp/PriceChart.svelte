b6e6c10728215f70cce81f2948b3bbf248b51e5b
Gobeli <etienne.gobeli@students.fhnw.ch>
2021-04-17 13:29:48 +0200

    add loader component

--- src/pages/comp/PriceChart.svelte
+++ src/pages/comp/PriceChart.svelte
@@ -3,6 +3,7 @@
   import dateFormat from "dateformat";
 
   import TimeFrameSelector from "./TimeFrameSelector.svelte";
+  import Loader from "./Loader.svelte";
   import { getDataURI } from "../../API/BitrawAPI";
   let tables = ["perc_75", "median_fee", "perc_25", "min_fee"];
 
@@ -140,26 +141,7 @@
     </div>
   {:else}
     <div class="flex-grow place-items-center">
-      <svg
-        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        />
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        />
-      </svg>
+      <Loader />
     </div>
   {/if}
 </div>
