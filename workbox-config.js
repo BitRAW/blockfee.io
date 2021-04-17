478dbd82456fd9d409a636a5c323128d066290e5
Gobeli <etienne.gobeli@students.fhnw.ch>
2021-04-17 12:56:53 +0200

    add api runtime cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{css,js,ico,html,svg,json}"
+  globDirectory: 'public/',
+  globPatterns: [
+    '**/*.{css,js,ico,html,svg,json}'
   ],
-  "swDest": "public/sw.js"
+  swDest: 'public/sw.js',
+  runtimeCaching: [{
+    urlPattern: new RegExp('https://backend.bitraw.io'),
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'api-calls',
+      cacheableResponse: { statuses: [0, 200] },
+      expiration: {
+        maxEntries: 20,
+      },
+    },
+  }],
 };
