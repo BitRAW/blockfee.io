f3aaf1134bd0ab17aa0c8c067a1877e4ba5e720e
Gabriel Comte <comte@puzzle.ch>
2021-05-05 12:10:01 +0200

    Short term chart now shows 4 hours instead of 6

--- src/util/infoTextUtils.js
+++ src/util/infoTextUtils.js
@@ -1,4 +1,4 @@
-const text6Hrs =
-  ' Only the 6 hours chart contains unagregated data. For the other charts, the avarage is taken to make the chart more readable.'
+const text4Hrs =
+  ' Only the 4 hours chart contains unagregated data. For the other charts, the avarage is taken to make the chart more readable.'
 
-export { text6Hrs }
+export { text4Hrs }
