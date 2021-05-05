6c07f28a64ecdfd72d88661ee3a581c34ee52cdf
Gabriel Comte <comte@puzzle.ch>
2021-05-05 12:11:48 +0200

    Tooltips: Fix typo

--- src/util/infoTextUtils.js
+++ src/util/infoTextUtils.js
@@ -1,4 +1,4 @@
 const text4Hrs =
-  ' Only the 4 hours chart contains unagregated data. For the other charts, the avarage is taken to make the chart more readable.'
+  ' Only the 4 hours chart contains unaggregated data. For the other charts, the avarage is taken to make the chart more readable.'
 
 export { text4Hrs }
