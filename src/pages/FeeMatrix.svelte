77fe41da30b26477299a10600032bcb447354fe6
Etienne Gobeli <gobeli@puzzle.ch>
2021-06-29 13:06:18 +0200

    add fee matrix with monthly lookback

--- src/pages/FeeMatrix.svelte
+++ src/pages/FeeMatrix.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import {onMount} from 'svelte';
-import {setDay, setHours, setMinutes, subMinutes, getDay, getHours, subSeconds, setSeconds} from 'date-fns';
+import {setDay, setHours, setMinutes, subMinutes, getDay, getHours, setSeconds} from 'date-fns';
 
 import {
   fetchData,
@@ -8,21 +8,30 @@ import {
 import {weekdaysMap} from '../util/matrixUtils';
 import MatrixCell from './comp/MatrixCell.svelte';
 import Loader from './helper-comp/Loader.svelte';
+import TimeFrameSelector from './comp/TimeFrameSelector.svelte';
+
 import type {MatrixDay} from '../objects/MatrixDay';
 import Tippy from './helper-comp/Tippy.svelte';
 
+let data: Promise<Array<Array<MatrixDay>>> = null;
+let timeframe = 'Yearly lookback';
+let maxValue: number = 100;
 
-let matrix: Promise<Array<Array<MatrixDay>>> = null;
 const infoElementId : string = 'feematrix-info';
-const infoContent : string = 'Each block in the blockchain has a median fee rate. This matrix shows aggregates of these median fee rates. They are grouped according to when exactly a block was mined within a week, over the time span of a year. The averages of those values are what is displayed in the chart. They are denominated in satoshis/virtualByte. The data is continuously being updated and always represents the most recent 12 months.';
+const infoContent : string = 'Each block in the blockchain has a median fee rate. This matrix shows aggregates of these median fee rates. They are grouped according to when exactly a block was mined within a week, over the time span of a year. The averages of those values are what is displayed in the chart. They are denominated in satoshis/virtualByte. The data is continuously being updated and always represents the most recent month or 12 months, depending on the selected matrix.';
 
+const timeframeMap = {
+  'Yearly lookback': 'y',
+  'Monthly lookback': 'M',
+};
 
-onMount(() => {
-  let date = new Date();
-  const offsetMin = date.getTimezoneOffset();
-  matrix = fetchData<Array<Array<MatrixDay>>>('fees/matrix/weekhour')
+async function fetchMatrix(_timeframe = 'Yearly lookback') {
+  timeframe = _timeframe;
+  data = fetchData<Array<Array<MatrixDay>>>('fees/matrix/weekhour', {timeframe: timeframeMap[timeframe]})
       .then((data) => {
-        return data.flatMap((day) => day.map((hour) => {
+        let date = new Date();
+        const offsetMin = date.getTimezoneOffset();
+        const flatData = data.flatMap((day) => day.map((hour) => {
           date = setDay(date, hour.weekday);
           date = setHours(date, hour.hour);
           date = setMinutes(date, 0);
@@ -35,8 +44,11 @@ onMount(() => {
           hour.weekday = hour.weekday === 0 ? 7 : hour.weekday;
 
           return hour;
-        }),
-        ).sort((a, b) => a.weekday === b.weekday ?
+        }));
+
+        maxValue = Math.max(...flatData.map((d) => d.value));
+
+        return flatData.sort((a, b) => a.weekday === b.weekday ?
           a.hour - b.hour : a.weekday - b.weekday,
         ).reduce((acc, value) => {
           const dayIndex = value.weekday - 1;
@@ -47,12 +59,15 @@ onMount(() => {
           return acc;
         }, []);
       });
-});
+}
 
+onMount(() => {
+  fetchMatrix();
+});
 </script>
 
-<div class="w-full h-full bg-gray-900 rounded-md shadow-mdm flex-col flex m-4 p-2 overflow-x-scroll lg:overflow-x-visible">
-    <div class="flex justify-between w-full pb-2">
+<div class="w-full h-full bg-gray-900 rounded-md shadow-mdm flex-col flex m-4 p-2 space-y-2 overflow-x-scroll lg:overflow-x-visible">
+    <div class="flex justify-between w-full">
         <p class="text-lg font-bold">
             Median Fee Rate: Hourly Distribution over Weekdays
         </p>
@@ -74,16 +89,19 @@ onMount(() => {
         <Tippy elementId={infoElementId} content={infoContent}></Tippy>
         </div>
     </div>
-    <div class="flex justify-between w-full pb-4">
+    <div class="flex justify-between w-full">
         <p>
-            Measured in sat/vByte, averaged out over the past 12 months
+            Measured in sat/vByte, averaged out over the past month or the past 12 months
         </p>
     </div>
-    {#if matrix}
-      <div class="grid grid-cols-25 gap-1  w-max lg:w-full">
-          {#await matrix}
-            <Loader></Loader>
-          {:then matrix}
+    <div class="w-full pb-4">
+      <TimeFrameSelector options={Object.keys(timeframeMap)} activeOption={timeframe} on:selectTime={(e) => fetchMatrix(e.detail)} />
+    </div>
+    <div class="grid grid-cols-25 gap-1 w-max lg:w-full">
+        {#if data}
+        {#await data}
+          <Loader></Loader>
+        {:then matrix}
           {#each matrix as weekday,i}
             {#if i===0}
               <div  class="bg-gray-800 rounded-sm text-center"></div>
@@ -96,10 +114,10 @@ onMount(() => {
               {#if j===0}
                 <div class="bg-gray-700 rounded-sm text-center">{weekdaysMap[`${feematrixElement.weekday}`]}</div>
               {/if}
-              <MatrixCell day={feematrixElement}></MatrixCell>
+              <MatrixCell maxValue={maxValue} day={feematrixElement}></MatrixCell>
             {/each}
           {/each}
-          {/await}
-      </div>
-    {/if}
+        {/await}
+        {/if}
+    </div>
 </div>
--- src/pages/comp/TimeFrameSelector.svelte
+++ src/pages/comp/TimeFrameSelector.svelte
@@ -1,71 +1,30 @@
 <script lang="ts">
-  import {onMount} from 'svelte';
-  import {createEventDispatcher} from 'svelte';
+  import {createEventDispatcher, onMount} from 'svelte';
 
   const dispatch = createEventDispatcher();
 
-  let activeElem;
-
   onMount(() => {
-    if (!activeElem) {
-      activeElem = '4h';
-      dispatch('selectTime', activeElem);
-    }
+    setActiveElem(activeOption);
   });
 
-  function setActiveElem(e) {
-    activeElem = e.target.innerHTML;
-    dispatch('selectTime', activeElem);
+  function setActiveElem(option: string) {
+    activeOption = option;
+    dispatch('selectTime', option);
   }
+
+  export let options: string[];
+  export let activeOption;
 </script>
 
 <div class="flex pr-2 text-xs font-bold">
-  <div
-    class:bg-cyan-500={activeElem !== '4h'}
-    class:bg-cyan-600={activeElem === '4h'}
-    on:click={setActiveElem}
-    class="bg-cyan-500 hover:bg-cyan-600 rounded-l-sm p-2 transition ease-in-out cursor-pointer"
-  >
-    4h
-  </div>
-  <div
-    class:bg-cyan-500={activeElem !== '24h'}
-    class:bg-cyan-600={activeElem === '24h'}
-    on:click={setActiveElem}
-    class="bg-cyan-500 hover:bg-cyan-600 p-2 transition ease-in-out cursor-pointer"
-  >
-    24h
-  </div>
-  <div
-    class:bg-cyan-500={activeElem !== '7d'}
-    class:bg-cyan-600={activeElem === '7d'}
-    on:click={setActiveElem}
-    class="bg-cyan-500 hover:bg-cyan-600 p-2 transition ease-in-out cursor-pointer"
-  >
-    7d
-  </div>
-  <div
-    class:bg-cyan-500={activeElem !== '30d'}
-    class:bg-cyan-600={activeElem === '30d'}
-    on:click={setActiveElem}
-    class="bg-cyan-500 hover:bg-cyan-600 p-2 transition ease-in-out cursor-pointer"
-  >
-    30d
-  </div>
-  <div
-    class:bg-cyan-500={activeElem !== '1y'}
-    class:bg-cyan-600={activeElem === '1y'}
-    on:click={setActiveElem}
-    class="bg-cyan-500 hover:bg-cyan-600  p-2 transition ease-in-out cursor-pointer"
-  >
-    1y
-  </div>
-  <div
-    class:bg-cyan-500={activeElem !== 'all'}
-    class:bg-cyan-600={activeElem === 'all'}
-    on:click={setActiveElem}
-    class="bg-cyan-500 hover:bg-cyan-600 rounded-r-sm p-2 transition ease-in-out cursor-pointer"
-  >
-    all
-  </div>
+  {#each options as option}
+    <div
+      class:bg-cyan-500={activeOption !== option}
+      class:bg-cyan-600={activeOption === option}
+      on:click={() => setActiveElem(option)}
+      class="bg-cyan-500 hover:bg-cyan-600 first:rounded-l-sm last:rounded-r-sm p-2 transition ease-in-out cursor-pointer"
+    >
+      {option}
+    </div>
+  {/each}
 </div>
